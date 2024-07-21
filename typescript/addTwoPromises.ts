bb073cdd4879e4383fdadb2e7bb6e7892f739585
Samet Palitci <abdulsametpalitci@gmail.com>
2024-07-21 21:55:20 +0300

    fix: add some QoL updates

--- typescript/addTwoPromises.ts
+++ typescript/addTwoPromises.ts
@@ -1,6 +1,4 @@
-type P = Promise<number>
-
-async function addTwoPromises(promise1: P, promise2: P): P {
+async function addTwoPromises(promise1: P, promise2: P): Promise<number> {
     const [p1, p2] = await Promise.all([promise1, promise2])
     return p1+p2
 };
