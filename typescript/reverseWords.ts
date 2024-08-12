5e3e6fc38e98bf6bc6512590b9e0637c142267f5
Samet Palitci <abdulsametpalitci@gmail.com>
2024-08-12 23:16:11 +0300

    fix: update reverse words

--- typescript/reverseWords.ts
+++ typescript/reverseWords.ts
@@ -1,3 +1,3 @@
 function reverseWords(s: string): string {
-    return s.split(" ").map((a)=>a.split("").reverse().join("")).join(" ")
+    return s.split(" ").map((a) => a.split("").reverse().join("")).join(" ")
 };
