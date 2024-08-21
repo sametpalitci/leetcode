9aa4e6288fbaeb83fd650e732882b7c3f33eedde
Samet Palitci <abdulsametpalitci@gmail.com>
2024-08-21 23:26:01 +0300

    fix: update big int answer

--- javascript/addBinary.js
+++ javascript/addBinary.js
@@ -1,6 +1,5 @@
 const addBinary = (a, b) => {
-    let total = BigInt("0b" + a) + BigInt("0b" + b);
-    return total.toString(2);
+    return (BigInt("0b" + a) + BigInt("0b" + b)).toString(2);
 };
 
 addBinary(11, 1);
