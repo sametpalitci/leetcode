83a3cfb4f1f78b282dee5f127aad966f60078aef
Samet Palitci <abdulsametpalitci@gmail.com>
2024-08-25 23:28:47 +0300

    fix: add some QoL updates

--- typescript/filterElementsfromArray.ts
+++ typescript/filterElementsfromArray.ts
@@ -1,5 +1,3 @@
-type Fn = (n: number, i: number) => any
-
-function filter(arr: number[], fn: Fn): number[] {
+function filter(arr: number[], fn: (n: number, i: number) => any): number[] {
 	return arr.filter((num, index)=> fn(num,index))
 };
